fc34676a4d0c7fc8507d7a3eccf0a125c05633cb
IreneY <gognofney@gmail.com>
2022-08-26 02:01:15 +0800

    Update 2022-08-25T18:01:13.272Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,23 +2,44 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
-});
-var swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar'
-  }
+  var swiper = new Swiper('.code-swiper', {
+    slidesPerView: 10,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 1500
+    },
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar'
+    }
+  });
+  new Swiper(".review-swiper", {
+    // autoplay: {
+    //   delay: 3000,
+    //   },
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
