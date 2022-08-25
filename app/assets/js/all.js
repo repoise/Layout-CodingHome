394b3acdd1f98614be5bc2268e415f7881a319c5
IreneY <gognofney@gmail.com>
2022-08-26 02:02:05 +0800

    課程介紹頁面、套用swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,26 +1,52 @@
 $(function() {
   console.log('Hello Bootstrap5');
+
+  const swiper = new Swiper('.code-swiper', {
+    
+    slidesPerView: 10,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 1500,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  new Swiper(".review-swiper", {
+    // autoplay: {
+    //   delay: 3000,
+    //   },
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
 });
 
        
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
